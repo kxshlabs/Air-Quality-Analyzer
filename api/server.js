f5e96bf560f90c57c54ce47957c60226c39f308a
kxshlabs <kushthoke@gmail.com>
2026-09-16 12:19:26 +0530

    feat: Changes in Server.js

--- api/server.js
+++ api/server.js
@@ -4,7 +4,9 @@
  */
 
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "../.env") });
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: path.join(__dirname, "../.env") });
+}
 
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
